a30465bd4e45b93fd3b54f4a78d59833f57a5f28
Scotty Waggoner <ozzieorca@gmail.com>
2017-07-19 12:36:08 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const concat = require('lodash/concat');
+const omit = require('lodash/omit');
+const keys = require('lodash/keys');
+
+const isBuild = process.env.npm_lifecycle_event === 'build';
+
+const entryPoints = {
+  main: './main.js'
+};
+
+
+module.exports = {
+  entry: entryPoints,
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    devtoolModuleFilenameTemplate: info => info.resourcePath.replace(/^\.\//, '')
+  },
+  plugins: concat(
+    [
+      new ExtractTextPlugin({ filename: "[name].[chunkhash].css", disable: !isBuild }),
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      })
+    ]
+  ),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['env', { "modules": false }]],
+              plugins: ['transform-runtime']
+            }
+          }
+        ]
+      },
+      // extract global css into separate files
+      {
+        test: /\.scss$/,
+        use: isBuild ?
+          ExtractTextPlugin.extract({
+            use: [
+              "css-loader?sourceMap",
+              "sass-loader?sourceMap"
+            ]
+          }) :
+          [
+            "style-loader",
+            "css-loader?sourceMap",
+            "sass-loader?sourceMap"
+          ]
+      },
+      {
+        test: /\.(json|woff|ttf|eot|ico)/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: "/dist/",
+    historyApiFallback: true
+  }
+};
